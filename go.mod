710aeb40d309c078581f6b445a19334eb1e8c2ed
jaronoff97 <jacobaronoff45@gmail.com>
2026-01-21 13:56:47 -0500

    chore: init repo

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/usetero/policy-go
+
+go 1.25.6
---
+++ policy.go
@@ -0,0 +1,8 @@
+// Package policy implements the Tero Policy Specification for high-performance
+// log policy evaluation and transformation.
+package policy
+
+// Version returns the current version of the policy library.
+func Version() string {
+	return "0.1.0"
+}
